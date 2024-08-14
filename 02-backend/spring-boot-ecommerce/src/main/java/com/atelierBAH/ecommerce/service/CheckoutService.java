e183a2f29a1609d3699d7b7c1046534f4fa79ca6
hmytt <99262619+hmytt@users.noreply.github.com>
2024-08-14 23:03:34 +0200

    Delete 02-backend/spring-boot-ecommerce directory

--- 02-backend/spring-boot-ecommerce/src/main/java/com/atelierBAH/ecommerce/service/CheckoutService.java
+++
@@ -1,9 +0,0 @@
-package com.atelierBAH.ecommerce.service;
-
-import com.atelierBAH.ecommerce.dto.Purchase;
-import com.atelierBAH.ecommerce.dto.PurchaseResponse;
-
-public interface CheckoutService {
-
-    PurchaseResponse placeOrder(Purchase purchase);
-}
